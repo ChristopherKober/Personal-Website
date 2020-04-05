00c3e17cbde8070cb4206c8976b3834ad60c0b06
Christopher Kober <ChristopherKober@users.noreply.github.com>
2020-04-05 19:06:31 -0400

    Add initial fantasy football app build

---
+++ projects/fantasy-football/precache-manifest.404080188507d32c1addfac0d8b45002.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "173d45966ccd33178e15c74f08c5398b",
+    "url": "/projects/fantasy-football/index.html"
+  },
+  {
+    "revision": "533726ca0db03687cf10",
+    "url": "/projects/fantasy-football/static/css/main.68b41843.chunk.css"
+  },
+  {
+    "revision": "936903123c02e2bfe827",
+    "url": "/projects/fantasy-football/static/js/2.ab178330.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projects/fantasy-football/static/js/2.ab178330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "533726ca0db03687cf10",
+    "url": "/projects/fantasy-football/static/js/main.6989c764.chunk.js"
+  },
+  {
+    "revision": "78d0c6ff91932cc699a1",
+    "url": "/projects/fantasy-football/static/js/runtime-main.d7c177e8.js"
+  }
+]);
