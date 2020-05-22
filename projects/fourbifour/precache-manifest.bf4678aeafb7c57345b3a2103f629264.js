054df54508121ea7dd66b2abaa89613fdcd2f98c
Christopher Kober <ChristopherKober@users.noreply.github.com>
2020-05-22 01:19:53 -0400

    Add Fred Podcast project

---
+++ projects/fourbifour/precache-manifest.bf4678aeafb7c57345b3a2103f629264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090a9d86065224f38a8b3b23890e70af",
+    "url": "/projects/fourbifour/index.html"
+  },
+  {
+    "revision": "8a58308f45141a820d8a",
+    "url": "/projects/fourbifour/static/css/main.8151b705.chunk.css"
+  },
+  {
+    "revision": "87a44522bb729865ab3f",
+    "url": "/projects/fourbifour/static/js/2.1fab7f78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects/fourbifour/static/js/2.1fab7f78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a58308f45141a820d8a",
+    "url": "/projects/fourbifour/static/js/main.1aa557f7.chunk.js"
+  },
+  {
+    "revision": "c50a43bb2048b9477be0",
+    "url": "/projects/fourbifour/static/js/runtime-main.10627943.js"
+  }
+]);
